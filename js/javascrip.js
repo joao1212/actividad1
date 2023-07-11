980d95e20048788d2489a041c1515f6a6addc92a
joao1212 <joaonavarromendoza@gmail.com>
2023-07-10 22:53:38 -0500

    proyecto completo html y js

---
+++ js/javascrip.js
@@ -0,0 +1,5 @@
+// Esta funcin se asocia con la line 85 de cntactenos.html
+function validacion_Datos() {
+    let nombre = document.getElementById("name").value;
+    alert("bienvenido al formulario " + nombre); 
+}
